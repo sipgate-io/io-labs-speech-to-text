7c0486aa1c95e68fc8690139f67ddb9a1608f8f6
Jannik Volkland <volkland@sipgate.de>
2021-10-26 15:29:57 +0200

    :rotating_light: add eslint config to project and fix warnings
    Co-authored-by: Benjamin Boll <boll@sipgate.de>

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import { createHistoryModule, sipgateIO } from 'sipgateio';
-import * as dot from 'dotenv';
-import {convertMp3ToText} from "./speechToTextEngine.js";
+import * as dot from "dotenv";
+import { createHistoryModule, sipgateIO } from "sipgateio";
+
+import convertMp3ToText from "./speechToTextEngine.js";
 
 dot.config();
 
@@ -8,18 +9,15 @@ const tokenId = process.env.SIPGATE_TOKEN_ID;
 const token = process.env.SIPGATE_TOKEN;
 
 const client = sipgateIO({
-    tokenId: tokenId,
-    token: token,
+  tokenId,
+  token,
 });
 
 const historyClient = createHistoryModule(client);
 
-historyClient.fetchAll({types: ['VOICEMAIL']},{limit: 1})
-    .then(allVoicemails => {
-        allVoicemails.forEach(voicemail => {
-            console.log(voicemail.recordingUrl);
-            convertMp3ToText(voicemail.recordingUrl)
-
-        })
-    }
-)
+historyClient.fetchAll({ types: ["VOICEMAIL"] }, { limit: 1 })
+  .then((allVoicemails) => {
+    allVoicemails.forEach((voicemail) => {
+      convertMp3ToText(voicemail.recordingUrl);
+    });
+  });
--- src/speechToTextEngine.js
+++ src/speechToTextEngine.js
@@ -1,53 +1,51 @@
-import * as vosk from "vosk";
 import { existsSync } from "fs";
 import { Readable } from "stream";
-import { Reader } from "wav";
+
 import ffmpeg from "fluent-ffmpeg";
+import * as vosk from "vosk";
+import { Reader } from "wav";
 
 const MODEL_PATH = "model";
 if (!existsSync(MODEL_PATH)) {
-	console.log(
-		"Please download the model from https://alphacephei.com/vosk/models and unpack as " +
-			MODEL_PATH +
-			" in the root folder."
-	);
-	process.exit();
+  console.log(`Please download the model from https://alphacephei.com/vosk/models and unpack as ${MODEL_PATH} in the root folder.`);
+  process.exit();
 }
 
-vosk.setLogLevel(0);
+vosk.setLogLevel(-1);
 const model = new vosk.Model(MODEL_PATH);
 
- const getWfReader = () => {
-	const wfReader = new Reader();
-	const wfReadable = new Readable().wrap(wfReader);
+const getWfReader = () => {
+  const wfReader = new Reader();
+  const wfReadable = new Readable().wrap(wfReader);
 
-	wfReader.on("format", async ({ sampleRate }) => {
-		const rec = new vosk.Recognizer({
-			model: model,
-			sampleRate: sampleRate,
-		});
-		for await (const data of wfReadable) {
-			rec.acceptWaveform(data);
-		}
-		console.log("Voicemail content: \""+rec.finalResult().text+"\"");
-		rec.free();
-	});
-	return wfReader;
+  wfReader.on("format", async ({ sampleRate }) => {
+    const rec = new vosk.Recognizer({
+      model,
+      sampleRate,
+    });
+    for await (const data of wfReadable) {
+      rec.acceptWaveform(data);
+    }
+    console.log(`Voicemail content: "${rec.finalResult().text}"`);
+    rec.free();
+  });
+  return wfReader;
 };
 
-export const convertMp3ToText = (path) =>
-	ffmpeg(path)
-		.audioFrequency(16000)
-		.toFormat("wav")
-		.on("error", (err) => {
-			console.log(
-				"An error occurred while converting the stream: " + err.message
-			);
-		})
-		.on("progress", (progress) => {
-			// console.log("Processing: " + progress.targetSize + " KB converted");
-		})
-		.on("end", () => {
-			// console.log("Processing finished !");
-		})
-		.pipe(getWfReader());
+const convertMp3ToText = (path) => ffmpeg(path)
+  .audioFrequency(16000)
+  .toFormat("wav")
+  .on("error", (err) => {
+    console.log(
+      `An error occurred while converting the stream: ${err.message}`,
+    );
+  })
+  .on("progress", (progress) => {
+    console.log(`Processing: ${progress.targetSize} KB converted`);
+  })
+  .on("end", () => {
+    console.log("Processing finished!");
+  })
+  .pipe(getWfReader());
+
+export default convertMp3ToText;
